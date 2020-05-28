9cc7a0f060890b860ba88637c18109bb44d0455e
Nikita <latinisnotdead07@gmail.com>
2020-05-28 17:15:52 +0100

    refactor to use the PRG pattern

--- app.rb
+++ app.rb
@@ -4,15 +4,23 @@ class Battle < Sinatra::Base
 
   # set :root, File.battle(__FILE__)
 
-  # enable :sessions
+  enable :sessions
 
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @player_1 = params[:Player_1]
-    @player_2 = params[:Player_2]
+    session[:player_1] = params[:Player_1]
+    #session.store(:player_1, @player_1)
+    session[:player_2] = params[:Player_2]
+    #session.store(:player_2, @player_2)
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1 = session[:player_1]
+    @player_2 = session[:player_2]
     erb(:play)
   end
 
