824b535efaf5b6e77eaed89ea1daf6f5dd158b74
Sumner1185 <alastair.sumner@yahoo.com>
2020-05-28 14:49:51 +0100

    Set up file structure and add sinatra and RSpec

---
+++ app.rb
@@ -0,0 +1,16 @@
+require 'sinatra/base'
+
+class Battle < Sinatra::Base
+
+  # set :root, File.battle(__FILE__)
+
+  # enable :sessions
+
+  get '/' do
+    "Hello Battle!"
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+
+end
